ad02ecf9e7483568dfd96bbdd4f79da2fe9e31af
shaito <shaito@defi.sucks>
2024-03-11 15:38:10 -0300

    feat: local server

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import { JSONRPCServer } from "json-rpc-2.0";
+import express from "express";
+import bodyParser from "body-parser";
+
+const PORT = 5555;
+const app = express();
+app.use(bodyParser.json());
+
+const server = new JSONRPCServer();
+
+server.addMethod("getSqrt", async (params) => {
+  const values = params[0].Array.map(({ inner }) => {
+    return { inner: `${Math.sqrt(parseInt(inner, 16))}` };
+  });
+  return { values: [{ Array: values }] };
+ });
+
+app.post("/", (req, res) => {
+ const jsonRPCRequest = req.body;
+ server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+  if (jsonRPCResponse) {
+   res.json(jsonRPCResponse);
+  } else {
+   res.sendStatus(204);
+  }
+ });
+});
+
+app.listen(PORT, () => {
+    console.log(`Oracle running at port: ${PORT}`)
+})
